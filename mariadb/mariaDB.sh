9533cdb779e25081fa5a2a22963f9c39ffa1e48c
siru02 <khm32323@gmail.com>
2024-08-01 19:47:35 +0900

    버그

---
+++ mariadb/mariaDB.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# 초기화 여부 확인
+INIT_FLAG="/var/lib/mysql/.initialized"
+
+# 데이터 베이스 접속 정보 설정
+DB_HOST="localhost"
+DB_PORT="3306"
+DB_USER="root"
+DB_PASS="abcd"
+DB_NAME=""
+
+# 새로운 DB사용자 정보 설정
+WD_NAME="wordpress"
+WD_USER="hgu"
+WD_USER_PASS="1234"
+
+if [ ! -f "$INIT_FLAG" ]; then
+    # mysql 폴더 생성 및 권한 설정
+    if [ ! -d "/var/lib/mysql/mysql" ]; then
+        mkdir -p /var/lib/mysql
+        chown -R mysql:mysql /var/lib/mysql
+        chmod -R 755 /var/lib/mysql
+    fi
+
+	# MariaDB 데이터 디렉토리 초기화 / 이미 존재하면 실행되지 않으니 항상해도 됨
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+    # MariaDB 백그라운드에서 실행 -> 테스트에서는 포그라운드에서 실행하도록
+    /usr/bin/mysqld_safe --user=mysql --datadir=/var/lib/mysql &
+
+    # MariaDB 서버가 시작될 때까지 대기
+    while ! mysqladmin ping --silent; do
+        sleep 1
+    done
+
+    # MariaDB 접속 및 명령어 실행
+    mysql -u $DB_USER -p$DB_PASS << EOF
+
+    -- root계정 비밀번호 추가
+    SET PASSWORD FOR root@localhost= PASSWORD("$DB_PASS");
+
+    -- 빈유저 삭제
+    USE mysql;
+    DELETE FROM user WHERE User = '';
+
+    -- 워드프레스 db생성 및 관리자 추가
+    CREATE DATABASE IF NOT EXISTS $WD_NAME;
+    CREATE USER IF NOT EXISTS '$WD_USER'@'localhost' IDENTIFIED BY '$WD_USER_PASS';
+    GRANT ALL PRIVILEGES ON $WD_NAME.* TO '$WD_USER'@'localhost';
+    FLUSH PRIVILEGES;
+
+    -- 접속 종료
+EOF
+
+    # 초기화 완료 플래그 생성
+    touch "$INIT_FLAG"
+
+    # MariaDB 종료
+    mysqladmin -u root -pabcd shutdown
+fi
+
+echo "building success";
+
+# MariaDB 실행 (포그라운드)
+exec /usr/bin/mysqld_safe --user=mysql --datadir=/var/lib/mysql
