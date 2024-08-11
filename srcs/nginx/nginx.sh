673404f7af0e5e7e0118bad4251af694d33ffb28
siru02 <khm32323@gmail.com>
2024-08-11 19:27:32 +0900

    f

--- srcs/nginx/nginx.sh
+++ srcs/nginx/nginx.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 # 초기화 여부 확인 
-INIT_FLAG="/.initialized"
 
 # wordpress, db컨테이너를 위해 대기
 echo "Waiting db, wordpress"
 
-if [ ! -f "$INIT_FLAG" ]; then
-    touch $INIT_FLAG
+if [ ! -f "$NGINX_INIT_FLAG" ]; then
+    touch $NGINX_INIT_FLAG
 
-    # mv /etc/nginx/fastcgi.conf /etc/nginx/http.d 
+    mv /etc/nginx/fastcgi.conf /etc/nginx/http.d 
     if [ ! -f /etc/ssl/private/nginx-selfsigned.key ]; then
     # keyout: 생성된 private키를 저장하는 경로, out: 생성된 인증서를 저장하는 경로, subj: 인증서의 주체에 대한 정보
         openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
