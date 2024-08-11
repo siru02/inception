4991d221aa5d97dea4b3aad500f10f91899951c7
siru02 <khm32323@gmail.com>
2024-08-11 18:24:37 +0900

    f

--- srcs/nginx/nginx.sh
+++ srcs/nginx/nginx.sh
@@ -14,7 +14,7 @@ if [ ! -f "$INIT_FLAG" ]; then
         openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
             -keyout /etc/ssl/private/nginx-selfsigned.key \
             -out /etc/ssl/certs/nginx-selfsigned.crt \
-            -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul"
+            -subj "/C=KO/ST=Seoul/L=Gaepo/O=42Seoul/OU=hgu/CN=hgu.42.fr"
     fi
 fi
 
